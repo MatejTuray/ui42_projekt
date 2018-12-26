01807a25252e97950f6ba95ade029df613744d03
unknown <matej.turay@gmail.com>
2018-12-26 17:13:44 +0100

    prerender

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(publicPath))
-app.use(require('prerender-node'));
+app.use(require('prerender-node').set('prerenderToken', 'Yjb8DTXJwn9EQx7jFvEz'));
 app.get("*", (req, res) => {
     res.sendFile(path.join(publicPath, "index.html"))
 })
